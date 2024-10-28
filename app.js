2a438187eeb2c54e4abd8264c1da5a3274a3ac66
YonugJones <peterckerfoot@gmail.com>
2024-10-28 14:58:17 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const path = require("node:path");
+require('dotenv').config();
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
