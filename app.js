5d643550b13480cfa468fd0ea90662268a200dd4
YonugJones <peterckerfoot@gmail.com>
2024-10-31 12:20:49 -0500

    added passport de / serialize functions added localstrategy to app.js file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const path = require("node:path");
 const indexRouter = require('./routes/indexRouter');
+const pool = require('./pool');
 require('dotenv').config(); 
 
 const app = express();
@@ -15,6 +16,40 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+)
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
 
 app.use('/', indexRouter);
 
