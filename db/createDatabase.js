59791b649898e7ec6b9070152e2b311702ff90ed
YonugJones <peterckerfoot@gmail.com>
2024-11-02 10:29:27 -0500

    changed createDatabase file to txt file

--- db/createDatabase.js
+++
@@ -1,8 +0,0 @@
-// CREATE TABLE users (
-//   id SERIAL PRIMARY KEY,
-//   firstName VARCHAR ( 50 ),
-//   lastName VARCHAR ( 50 ),
-//   username VARCHAR ( 50 ) UNIQUE NOT NULL,
-//   password VARCHAR ( 255 ) NOT NULL,
-//   membershipStatus BOOLEAN DEFAULT FALSE
-// );
