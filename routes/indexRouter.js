c36059321c9adde07d2f8be86afab3102867080b
YonugJones <peterckerfoot@gmail.com>
2024-11-04 14:17:12 -0600

    created error message partial view added validation and sanitization to sign up form

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,15 +1,18 @@
 const { Router } = require('express');
+const { body, validationResult } = require('express-validator');
 const indexRouter = Router();
 const userController = require('../controllers/userController');
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
 
-indexRouter.get('/', (req, res) => {
-  res.render('index', { user: req.user })
+// GET routes
+indexRouter.get('/', (req, res) => res.render('index', { user: req.user }));
+indexRouter.get('/log-in', (req, res) => res.render('log-in-form'));
+indexRouter.get('/sign-up', (req, res) => {
+  res.render('sign-up-form', { userData: {}, errors: [] })
 });
 
-indexRouter.get('/log-in', (req, res) => res.render('log-in-form'));
-
+// POST log-in
 indexRouter.post('/log-in', 
   passport.authenticate('local', {
     successRedirect: '/',
@@ -17,6 +20,7 @@ indexRouter.post('/log-in',
   })
 );
 
+// log-out route
 indexRouter.get('/log-out', (req, res, next) => {
   req.logout((err) => {
     if (err) {
@@ -26,30 +30,49 @@ indexRouter.get('/log-out', (req, res, next) => {
   })
 });
 
-indexRouter.get('/sign-up', (req, res) => res.render('sign-up-form'));
-
-indexRouter.post('/sign-up', async (req, res) => {
-  const { firstName, lastName, username, password } = req.body;
-
-  bcrypt.hash(password, 10, async (err, hashedPassword) => {
-    if (err) {
-      console.error('Error hashing password:', err);
-      return res.status(500).send('Internal server error');
-    }
-
-    try {
-      await userController.addUser({
-        firstName,
-        lastName,
-        username,
-        password: hashedPassword
+// POST sign-up with sanitization and validation
+indexRouter.post(
+  '/sign-up', 
+  [
+    body('firstName').trim().isAlpha().escape().withMessage('First name should only contain letters'),
+    body('lastName').trim().isAlpha().escape().withMessage('Last name should only contain letters'),
+    body('username').trim().isLength({ min: 2 }).escape().withMessage('Username must be at least 2 characters.'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters.'),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords do not match')
+      }
+      return true;
+    })
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log('Validation errors:', errors.array())
+      return res.render('sign-up-form', {
+        errors: errors.array(),
+        userData: req.body
       });
-
-      res.redirect('/');
-    } catch (err) {
-      console.error('Error adding user to database:', err);
-      res.status(500).send('Internal server error');
     }
+
+    const { firstName, lastName, username, password } = req.body;
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      if (err) {
+        return res.status(500).send('Internal server error');
+      }
+
+      try {
+        await userController.addUser({
+          firstName,
+          lastName,
+          username,
+          password: hashedPassword
+        });
+        res.redirect('/');
+      } catch (err) {
+        console.error('Error adding user to database:', err);
+        res.status(500).send('Internal server error');
+      }
   });
 });
 
